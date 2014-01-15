767e192305dc4a749a0e27c9534cd006de404be7
Florian Friedrich <florian.friedrich@friesoft.ch>
2014-01-15 15:08:44 +0100

    Added clear method
    Added isRunning
    
    Added constraints to StoryBoard

--- DogeKitTestProject/DogeKitTestProject/DKViewController.m
+++ DogeKitTestProject/DogeKitTestProject/DKViewController.m
@@ -8,6 +8,7 @@
 
 #import "DKViewController.h"
 #import "DogeKit.h"
+
 @interface DKViewController ()
 @property (nonatomic, strong) DogeKit *dk;
 @end
@@ -20,20 +21,35 @@
 	// Do any additional setup after loading the view, typically from a nib.
     
     self.dk = [[DogeKit alloc] initWithTargetView:self.view];
+    [self.dk addObserver:self forKeyPath:@"isRunning" options:kNilOptions context:nil];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)dealloc {
+    [self.dk removeObserver:self forKeyPath:@"isRunning"];
 }
 
 - (IBAction)startDogeKit:(id)sender {
     [self.dk start];
-
 }
 
 - (IBAction)stopDogeKit:(id)sender {
     [self.dk stop];
 }
+
+- (IBAction)clearDogeKit:(id)sender {
+    [self.dk clear];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if (object == self.dk && [keyPath isEqualToString:@"isRunning"]) {
+        if (self.dk.isRunning) {
+            NSLog(@"Is running");
+        } else {
+            NSLog(@"Is not running");
+        }
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
 @end
